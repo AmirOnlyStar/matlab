3b6f4deedaebbd768e4f8b4ee82b6400b8a2f9aa
AmirOnlyStar <31685810+AmirOnlyStar@users.noreply.github.com>
2021-02-15 08:57:59 +0330

    math

--- lpf_filter.m
+++ lpf_filter.m
@@ -4,11 +4,11 @@ Fs = 100;
 T = 1/Fs;
 t = 0:T:1-T;
 s = sin(2*pi*10*t);
-plot(s,'r');
+% plot(s,'r');
 hold on;
 noise = 0.5*randn(size(t));
 x = s + noise;
-plot(x,'g');
+% plot(x,'g');
 
 Fc = 10;
 omega = 2*pi*Fc;
@@ -19,15 +19,15 @@ Hd = c2d(H,0.01)
 
 y=0;
 
-for i = 1:length(x)
-    y(i+1) = (y(i)*0.501)+(x(i)*0.499);
-end    
-plot(y,'b');
-
-% d = designfilt('lowpassfir','filterorder',5,'CutoffFrequency',5,'SampleRate',Fs);
+% for i = 1:length(x)
+%     y(i+1) = (y(i)*0.501)+(x(i)*0.499);
+% end    
 % plot(y,'b');
-% y = filter(d,x);
-% plot(y,'k');
+
+d = designfilt('lowpassfir','filterorder',5,'CutoffFrequency',5,'SampleRate',Fs);
+plot(x,'b');
+y = filter(d,x);
+plot(y,'Y');
 
 % nfft = length(s);
 % nfft2 = 2^nextpow2(nfft);
---
+++ myfun.m
@@ -0,0 +1,5 @@
+function a = myfun(b)
+
+a = b+10;
+
+end 
